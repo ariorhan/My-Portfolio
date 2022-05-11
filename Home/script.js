1ee8b1d6f8f026f3316706d64afc7badade5ce50
Orhan ARI <66368354+ariorhan@users.noreply.github.com>
2022-05-11 22:58:31 +0100

    Add files via upload

---
+++ Home/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    //hamburger Toggle
+    $('.hamburger').click(function(event){
+    $('.menu-list').slideToggle(500);
+    event.preventDefault();
+    
+    $('.menu-list1 li a').click(function(event) {
+        if ($(window).width() < 768) {
+          $('.menu-list').slideUp(500);
+          event.preventDefault(); 
+        }
+      });
+});
+
+
+});
